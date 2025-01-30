ec830e0fa2d678a89377a139f4b050c4341a522a
Rafael Mendes <raffaell_mendez@hotmail.com>
2025-01-30 08:05:36 -0300

    Recuperar registros do banco de dados

--- app/adms/Controllers/users/ListUsers.php
+++ app/adms/Controllers/users/ListUsers.php
@@ -4,14 +4,30 @@ namespace App\adms\Controllers\users;
 
 use App\adms\Models\Repository\UsersRepository;
 
+/**
+ * Controller listar usuários
+ * 
+ * @author Rfael Mendes <raffaell_mendez@hotmail.com>
+ */
 class ListUsers
 {
+    /** @var array|string|null $data Recebe os dados que devem ser enviados para a VIEW */
+    private array|string|null $data = null;
+
+    /**
+     * Recuperar os últimos usuários
+     * 
+     * @return void
+     */
     public function index()
     {
         echo "Listar Usuários<br>";
 
+        // Instanciar o Repository para recuperar os registros do banco de dados
         $listUsers = new UsersRepository();
-        $listUsers->getAllUsers();
+        $this->data['users'] = $listUsers->getAllUsers();
+
+        var_dump($this->data);
     }
       
 }
--- app/adms/Models/Repository/UsersRepository.php
+++ app/adms/Models/Repository/UsersRepository.php
@@ -3,14 +3,32 @@
 namespace App\adms\Models\Repository;
 
 use App\adms\Models\Services\DbConnection;
+use PDO;
 
+/**
+ * Repository responsável em buscar os usuários no banco de dados
+ * 
+ * @return Rafael Mendes
+ */
 class UsersRepository extends DbConnection
 {
-
+    /**
+     * Recuperar os registros
+     * @return array Usuários reguperados do banco de dados
+     */
     public function getAllUsers()
     {
-    //    $conexao = new DbConnection();
-    //    $conexao->getConnection();
-    $this->getConnection();
+        // QUERY para recuperar os registros do banco de dados
+        $sql = 'SELECT id, name, email, username FROM adms_users ORDER BY id DESC';
+
+        // Preparar a QUERY
+        $stmt = $this->getConnection()->prepare($sql);
+
+        // Executar a QUERY
+        $stmt->execute();
+
+        // Ler os registros e retornar
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     }
 }
