54414cc21be9206e1f20e77926e47bba106259c8
Rafael Mendes <raffaell_mendez@hotmail.com>
2025-02-12 10:06:42 -0300

    Criar formulário novo usuário

--- app/adms/Controllers/Services/LoadPageAdm.php
+++ app/adms/Controllers/Services/LoadPageAdm.php
@@ -25,7 +25,7 @@ class LoadPageAdm
     private string $classLoad;
 
     /** @var array $listPgPublic Recebe a lista de paginas publicas */
-    private array $listPgPublic = ["Login", "Error403"];
+    private array $listPgPublic = ["Login", "Error403", "NewUser"];
 
     /** @var array $listPgPrivate Recebe a lista de paginas privadas */
     private array $listPgPrivate = ["Dashboard", "ListUsers", "ViewUser", "CreateUser", "UpdateUser", "DeleteUser", "UpdatePasswordUser", "Logout"];
