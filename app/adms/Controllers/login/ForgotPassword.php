86ebf38ee2abd1d9bf1dba99440ba461d59756be
Rafael Mendes <raffaell_mendez@hotmail.com>
2025-02-12 16:04:46 -0300

    Gerar chave recuperar senha

--- app/adms/Controllers/login/ForgotPassword.php
+++ app/adms/Controllers/login/ForgotPassword.php
@@ -2,12 +2,15 @@
 
 namespace App\adms\Controllers\login;
 
+use App\adms\Controllers\Services\GenerateKeyService;
 use App\adms\Controllers\Services\Validation\ValidationEmailService;
 use App\adms\Helpers\CSRFHelper;
 use App\adms\Helpers\GenerateLog;
+use App\adms\Models\Repository\LoginRepository;
 use App\adms\Models\Repository\ResetPasswordRepository;
 use App\adms\Models\Repository\UsersRepository;
 use App\adms\Views\Services\LoadViewService;
+use PDO;
 
 class ForgotPassword
 {
@@ -74,5 +77,33 @@ class ForgotPassword
             return;
         }
 
+        // Instanciar o serviço para gerar a chave
+        $valueGenerateKey = GenerateKeyService::generateKey();
+
+        $this->data['form']['key'] = $valueGenerateKey['key'];
+        $this->data['form']['recover_password'] = $valueGenerateKey['encryptedkey'];
+
+        // Instanciar Repository para resetar a senha
+        $userUpdate = new ResetPasswordRepository();
+        $result = $userUpdate->updateForgotPassword($this->data['form']);
+
+        // Acessa o IF se o repository retornou TRUE
+        if($result){
+            // Criar a mensagem de sucesso
+            $_SESSION['success'] = "Enviado email com as instruções para recuperar a senha. Acesse a sua caixa de email para recuperar a senha. - {$_ENV['URL_ADM']}reset-password/{$this->data['form']['key']}";
+
+            // Redirecionar o usuário para a pagina view - visualizar usuario
+            header("Location: {$_ENV['URL_ADM']}login");
+            return;
+        }else {
+            // Criar a mensagem de erro
+            $this->data['errors'][] = "Email com as instruções para recuperar a senha não enviado, tente novamente ou enrte em contato com o email {$_ENV['EMAIL_ADM']}";
+
+            // Chamar método carregar a view
+            $this->viewForgotPassword();
+        }
+
     }
+
+
 }
--- app/adms/Models/Repository/ResetPasswordRepository.php
+++ app/adms/Models/Repository/ResetPasswordRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\adms\Models\Repository;
 
+use App\adms\Helpers\GenerateLog;
 use App\adms\Models\Services\DbConnection;
+use Exception;
 use PDO;
 
 
@@ -27,5 +29,39 @@ class ResetPasswordRepository extends DbConnection
 
         // Ler o registro e retornar
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateForgotPassword(array $data): bool
+    {
+        // Usar try e catch para gerenciar exceção/erro
+
+        try { // Permanece no try se não houver nenhum erro
+
+            // QUERY para atualizar o usuário
+            $sql = 'UPDATE adms_users SET recover_password = :recover_password, validate_recover_password = :validate_recover_password,  updated_at = :updated_at';
+
+            // Condição para indicar qual registro editar
+            $sql .= ' WHERE email = :email LIMIT 1';
+
+            // Preparar a QUERY
+            $stmt = $this->getConnection()->prepare($sql);
+
+            // Substituir os links da QUERY pelo valor
+            $stmt->bindValue(':recover_password', $data['recover_password'], PDO::PARAM_STR);
+            $stmt->bindValue(':validate_recover_password', date("Y-m-d H:i:s", strtotime('+1hour')));
+            $stmt->bindValue(':updated_at', date("Y-m-d H:i:s"));
+            $stmt->bindValue(':email', $data['email'], PDO::PARAM_STR);
+
+
+            // Retornar TRUE quando conseguir executar a QUERY SQL, não considerando se alterou dados do registro
+            return $stmt->execute();
+
+        } catch (Exception $e) { // Acessa o catch quando houver erro no try
+
+            // Chamar o método para salvar o log
+            GenerateLog::generateLog("error", "Email para recuperar senha não encontrado.", ['email' => $data['email'], 'error' => $e->getMessage()]);
+
+            return false;
+        }
     }
 }
