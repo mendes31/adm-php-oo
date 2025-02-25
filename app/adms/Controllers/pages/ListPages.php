e088f94bb64efba056db4e793596bf5a3b6aafb1
Rafael Mendes <raffaell_mendez@hotmail.com>
2025-02-25 10:56:26 -0300

    Implementado Controllers, Models e Views para pages

---
+++ app/adms/Controllers/pages/ListPages.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\adms\Controllers\pages;
+
+use App\adms\Controllers\Services\PaginationService;
+use App\adms\Models\Repository\PagesRepository;
+use App\adms\Views\Services\LoadViewService;
+
+/**
+ * Controller para listar páginas
+ *
+ * Esta classe é responsável por recuperar e exibir uma lista de páginas no sistema. Utiliza um repositório
+ * para obter dados das páginas e um serviço de paginação para gerenciar a navegação entre páginas de resultados.
+ * Em seguida, carrega a visualização correspondente com os dados recuperados.
+ * 
+ * @package App\adms\Controllers\pages
+ * @author Rafael Mendes
+ */
+class ListPages
+{
+    /** @var array|string|null $data Dados que devem ser enviados para a VIEW */
+    private array|string|null $data = null;
+
+    /** @var int $limitResult Limite de registros por página */
+    private int $limitResult = 10; // Ajuste conforme necessário
+
+    /**
+     * Recuperar e listar páginas com paginação.
+     * 
+     * Este método recupera as páginas a partir do repositório de páginas com base na página atual e no limite
+     * de registros por página. Gera os dados de paginação e carrega a visualização para exibir a lista de páginas.
+     * 
+     * @param string|int $page Página atual para a exibição dos resultados. O padrão é 1.
+     * 
+     * @return void
+     */
+    public function index(string|int $page = 1): void
+    {
+        // Instanciar o Repository para recuperar os registros do banco de dados
+        $listPages = new PagesRepository();
+
+        // Recuperar as páginas para a página atual
+        $this->data['pages'] = $listPages->getAllPages((int) $page, (int) $this->limitResult);
+
+        // Gerar dados de paginação
+        $this->data['pagination'] = PaginationService::generatePagination(
+            (int) $listPages->getAmountPages(), 
+            (int) $this->limitResult, 
+            (int) $page, 
+            'list-pages'
+        );
+
+        // Definir o título da página
+        $this->data['title_head'] = "Listar Páginas";
+
+        // Ativar o item de menu
+        $this->data['menu'] = "list-pages";
+
+        // Carregar a VIEW com os dados
+        $loadView = new LoadViewService("adms/Views/pages/list", $this->data);
+        $loadView->loadView();
+    }
+}
---
+++ app/adms/Views/pages/create.php
@@ -0,0 +1,99 @@
+<?php
+
+use App\adms\Helpers\CSRFHelper;
+
+?>
+<div class="container-fluid px-4">
+
+    <div class="mb-1 hstack gap-2">
+        <h2 class="mt-3">Página</h2>
+
+        <ol class="breadcrumb mb-3 mt-3 ms-auto">
+            <li class="breadcrumb-item">
+                <a href="<?php echo $_ENV['URL_ADM']; ?>dashboard" class="text-decoration-none">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="<?php echo $_ENV['URL_ADM']; ?>list-pages" class="text-decoration-none">Páginas</a>
+            </li>
+            <li class="breadcrumb-item">Cadastrar</li>
+
+        </ol>
+
+    </div>
+
+    <div class="card mb-4 border-light shadow">
+
+        <div class="card-header hstack gap-2">
+            <span>Cadastrar</span>
+
+            <span class="ms-auto d-sm-flex flex-row">
+                <a href="<?php echo $_ENV['URL_ADM']; ?>list-pages" class="btn btn-info btn-sm me-1 mb-1"><i class="fa-solid fa-list"></i> Listar</a>
+            </span>
+
+        </div>
+
+        <div class="card-body">
+
+            <?php include './app/adms/Views/partials/alerts.php'; ?>
+
+            <!-- Formulário para cadastrar um novo página -->
+            <form action="" method="POST" class="row g-3">
+
+                <input type="hidden" name="csrf_token" value="<?php echo CSRFHelper::generateCSRFToken('form_create_page'); ?>">
+
+                <div class="col-12">
+                    <label for="name" class="form-label">Nome</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Nome da página" value="<?php echo $this->data['form']['name'] ?? ''; ?>">
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="controller" class="form-label">Controller</label>
+                    <input type="text" name="controller" class="form-control" id="controller" placeholder="Nome do método ou controller" value="<?php echo $this->data['form']['controller'] ?? ''; ?>">
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="controller_url" class="form-label">URL</label>
+                    <input type="text" name="controller_url" class="form-control" id="controller_url" placeholder="Nome do método ou controller na URL" value="<?php echo $this->data['form']['controller_url'] ?? ''; ?>">
+                </div>
+
+                <div class="col-12">
+                    <label for="directory" class="form-label">Diretório</label>
+                    <input type="text" name="directory" class="form-control" id="directory" placeholder="Nome do diretório da controller" value="<?php echo $this->data['form']['directory'] ?? ''; ?>">
+                </div>
+
+                <div class="col-12">
+                    <label for="obs" class="form-label">Observação</label>
+                    <textarea name="obs" class="form-control" id="obs" placeholder="Observação da página" rows="3"><?php echo $this->data['form']['obs'] ?? ''; ?></textarea>
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="page_status" class="form-label">Status</label>
+                    <input type="text" name="page_status" class="form-control" id="page_status" placeholder="Página ativa 1 e página inativa 0" value="<?php echo $this->data['form']['page_status'] ?? ''; ?>">
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="public_page" class="form-label">Pública</label>
+                    <input type="text" name="public_page" class="form-control" id="public_page" placeholder="Página publica 1 e página privada 0" value="<?php echo $this->data['form']['public_page'] ?? ''; ?>">
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="adms_packages_page_id" class="form-label">Pacote</label>
+                    <input type="text" name="adms_packages_page_id" class="form-control" id="adms_packages_page_id" placeholder="Pacote que está a página" value="<?php echo $this->data['form']['adms_packages_page_id'] ?? ''; ?>">
+                </div>
+
+                <div class="col-md-6 col-sm-12">
+                    <label for="adms_groups_page_id" class="form-label">Grupo</label>
+                    <input type="text" name="adms_groups_page_id" class="form-control" id="adms_groups_page_id" placeholder="Grupo que a página pertence" value="<?php echo $this->data['form']['adms_groups_page_id'] ?? ''; ?>">
+                </div>
+
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary btn-sm">Cadastrar</button>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
